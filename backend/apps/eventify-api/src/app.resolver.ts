aac84997bb371b6de7f96f52ae247f6f5d6dc6be
Azizbek Siddikov <azbeksid@gmail.com>
2025-04-28 18:30:02 +0900

    Resolved merge conflicts and deleted outdated files

--- backend/apps/eventify-api/src/app.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class AppResolver {
-	@Query(() => String)
-	public sayHello(): string {
-		return 'GraphQL API Server';
-	}
-}
